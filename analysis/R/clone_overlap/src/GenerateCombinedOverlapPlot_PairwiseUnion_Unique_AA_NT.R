c6ce2084515181afc3bcd65147d8ed28bff259b5
Pankaj Agarwal; p.agarwal@duke.edu; +1 919 681 6573 <agarw005@plp-lyerlab1.dhe.duke.edu>
2016-04-23 11:14:36 -0400

    Added code to get the unique numerical identifier of AA seq in combined sample overlap plots

--- analysis/R/clone_overlap/src/GenerateCombinedOverlapPlot_PairwiseUnion_Unique_AA_NT.R
+++ analysis/R/clone_overlap/src/GenerateCombinedOverlapPlot_PairwiseUnion_Unique_AA_NT.R
@@ -3,13 +3,14 @@ library(reshape2)
 library(scales)
 library(stringr)
 library(ggrepel)
-#filePairwise = "504_orienx010_baseline_AA_504_orienx010_post_AA.tsv"
-#filePairwise = "604_orienx010_baseline_AA_604_orienx010_post_AA.tsv"
+#can use the following file for testing
+#filePairwise = "AA_Treg_depleted_ExpU_E_214_tumor_AA_Treg_depleted_ExpU_E_224_tumor.tsv"
+#
 print("Begin...")
 inputdir = "/srv/agarw005/projects/tcr-seq/Erika_MouseCTLA_PD1/tcrseq-mouse-pd1ctla4-treatment/analysis/R/clone_overlap/data/input/combinedgroups/"
 outputdir = "/srv/agarw005/projects/tcr-seq/Erika_MouseCTLA_PD1/tcrseq-mouse-pd1ctla4-treatment/analysis/R/clone_overlap/data/results/combinedgroups/"
 cmdFlag = T # change to T if taking the name of the pairwise TSV file from command line
-
+#
 if(cmdFlag==T){
 args = commandArgs(trailingOnly=TRUE)
 numargs = length(args)
@@ -31,6 +32,14 @@ pos = unlist(str_locate_all(pattern =".tsv", filePairwise))[1]
 print(paste("pos=",pos))
 pairwiseSample = substr(filePairwise, 0, pos-1)
 print(paste("pairwiseSample = ", pairwiseSample))
+#
+# 04-21-16
+# read in the file with the list of unique AA clones across all samples which have a unique numerical identifier - use that for the label in the plot instead of the AA sequence
+# read in as factors - so can make sure number of levels same as number of row for uniqueness
+#
+dfUniqAALab = read.table("/srv/agarw005/projects/tcr-seq/Erika_MouseCTLA_PD1/tcrseq-mouse-pd1ctla4-treatment/analysis/R/data/uniqueCDR3List/AA_UniqueCDR3List.tsv", sep="\t", header=T)
+str(dfUniqAALab)
+#
 #
 ###stop("test stop")### For testing
 #
@@ -66,10 +75,17 @@ overlapPlot = overlapPlot + scale_x_continuous(labels = comma, breaks=seq(0,max,
 layer_abline = geom_abline(intercept=0, slope=1, color="blue", linetype="dashed", size=0.05)
 overlapPlot = overlapPlot + layer_abline
 #overlapPlot
+#create a data frame for putting text label in overlap plot - just a subset of the overlap pairwise data frame with the top 2% clones since only they will get the AA seq (or numerical identifier) label - 2% can be changed (parameterized?)
 dftext = subset(df, df[,3] > 0.02 | df[,5] > 0.02)
+#insert column with unique numerical identifer
+dftext$CDR3.UniqueAA.Seq.ID = subset(dfUniqAALab, dfUniqAALab$CDR3.UniqueAA.Seq %in% dftext$CDR3.UniqueAA.Seq,c("CDR3.UniqueAA.Seq.ID"))
 dftext
-layer_text = geom_text_repel(data = dftext, aes(x = dftext[,2], y = dftext[,4], label = dftext[,1]))
 #layer_text = geom_text(data = dftext, aes(x = dftext[,2], y = dftext[,4], label = dftext[,1]))
+# with repel
+#layer_text = geom_text_repel(data = dftext, aes(x = dftext[,2], y = dftext[,4], label = dftext[,1]))
+# with unique numerical label
+layer_text = geom_text_repel(data = dftext, aes(x = dftext[,2], y = dftext[,4], label = dftext[,"CDR3.UniqueAA.Seq.ID"]))
+#
 overlapPlot = overlapPlot + layer_text
 #overlapPlot
 overlapPlot = overlapPlot + ggtitle(pairwiseSample)
